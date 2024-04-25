8e913d0526c237c03e0ba918604e5435419f7a54
GustavoSouzaLobo <gustavosouzalobo@gmail.com>
2024-04-25 15:58:53 -0300

    Repositories and interfaces

--- src/main/java/com/gustavo/projeto/domain/Categoria.java
+++ src/main/java/com/gustavo/projeto/domain/Categoria.java
@@ -1,17 +1,34 @@
 package com.gustavo.projeto.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
+
+@Entity
+public class Categoria implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String descricao;
 	
 	private List<Livro> livros = new ArrayList<>();
-
+	
+	
+	@OneToMany(mappedBy = "categoria")
 	public Categoria() {
 		super();
 		// TODO Auto-generated constructor stub
